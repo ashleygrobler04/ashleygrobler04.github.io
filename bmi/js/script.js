52bc2dd8c8ac04e56d9e8a373ca12f6c725cba1d
ashleygrobler04 <ashleygrobler04@gmail.com>
2021-04-25 19:45:37 +0200

    updated the bmi-calculator.

--- bmi/js/script.js
+++ bmi/js/script.js
@@ -9,17 +9,23 @@ function display_bmi(){
     var height=document.getElementById("height").value;
     var weight=document.getElementById("weight").value;
     var finalbmi=calculate_bmi(weight,height);
-    document.getElementById("res").innerHTML="Your BMI is "+Math.round(finalbmi);
+    document.getElementById("res").innerHTML="Your BMI is "+finalbmi;
     if(finalbmi<18.5){
         document.getElementById("res").innerHTML+="<br/> You're under weight!";
     }
-    else if(finalbmi>=18.5 && finalbmi<=24.9){
+    else if(finalbmi>18.5 && finalbmi<24.9){
         document.getElementById("res").innerHTML+="<br/> Your weight is normal.";
     }
-    else if(finalbmi>=25.0 && finalbmi<=29.9){
+    else if(finalbmi>25.0 && finalbmi<29.9){
         document.getElementById("res").innerHTML+="<br/> You're over weight!";
     }
     else if (finalbmi>=30){
         document.getElementById("res").innerHTML+="<br/> You're obese...   ";
+    }
+    else if (finalbmi===18.5){
+        document.getElementById("res").innerHTML+="your weight is normal.";
+    }
+    else if (finalbmi===25.0){
+        document.getElementById("res").innerhtml+="you are over weight";
     }
 }
